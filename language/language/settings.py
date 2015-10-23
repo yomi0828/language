7da9882585929414673e825e977fa029a8252163
m516 <m516@m516>
2015-10-24 02:08:20 +0800

    first push

--- language/language/settings.py
+++ language/language/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'languageDB',
         'USER': 'language',
-        'PASSWORD': 'language',
+        'PASSWORD': 'xxxxxxxx',
         'HOST': 'localhost',
         'PORT': '',
     }
