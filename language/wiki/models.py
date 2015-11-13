0309ea5375dc25888e927445bc8146528dfb2458
m516 <m516@m516>
2015-11-13 16:28:53 +0800

    ch8 9

--- language/wiki/models.py
+++ language/wiki/models.py
@@ -5,7 +5,6 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-    slug = models.SlugField(unique=True)
     
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
@@ -22,6 +21,12 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
+    
+    
+    def save(self, *args, **kwargs):
+        self.title = self.title.replace(' ', '-')
+        super(Page, self).save(*args, **kwargs)
+
 
     def __str__(self):
         return self.title
